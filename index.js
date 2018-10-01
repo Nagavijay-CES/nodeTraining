b571229235786c1c2d773fd1eebd0966a592c7bf
Nagavijay Kolla <tirumalanagavijay.kolla@cesltd.com>
2018-10-01 14:37:36 +0530

    Assignment-1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var express= require('express');
+
 var http = require('http');
 var data ;
 
@@ -15,28 +15,4 @@ http.createServer(function(req,res){
     
 }).listen(5678)
 
-
-
 console.log('>>>>  HTTP Application Running successfully - Please Visit : http://127.0.0.1:5678');
-/*HTTP ENDS */
-
-/* ============================================================================================================= */
-
-/*With Express Server Framework  */
-
-app = express();
-
-var data  = 'Welcome Nagavijay';
-
-app.get('/',function(req,res){
-    res.send(data +' - Please use this (http://127.0.0.1:1234/dashboard) link to access dashboard Page');
-})
-
-app.get('/dashboard',function(req,res){
-      res.send(data +', Now you are in Dashboard page')
-})
-
-// Assigning Port 
-app.listen(1234);
-/*Express Server Framework ENDS */
-console.log('>>>>> Express.js Application Running successfully - Please Visit : http://127.0.0.1:1234');
