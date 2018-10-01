7ab240e291a2ea55307a77ec34fe056fadaec1ed
Nagavijay Kolla <tirumalanagavijay.kolla@cesltd.com>
2018-10-01 15:45:23 +0530

    "Reviewed by prakash"

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
 var express= require('express');
-// var http = require('http');
+
 var data ;
-
-// /*Normal HTTP  */
-// http.createServer(function(req,res){
-    
-//     if(req.url == '/home'){
-//         data = 'This is home page';
-//     }
-//     else if(req.url == '/dashboard'){
-//         data ='Hey it is dashboard page';
-//     }
-//     res.end(data);
-    
-// }).listen(5678)
-
-
-
-// console.log('>>>>  HTTP Application Running successfully - Please Visit : http://127.0.0.1:5678');
-// /*HTTP ENDS */
-
-/* ============================================================================================================= */
 
 /*With Express Server Framework  */
 
