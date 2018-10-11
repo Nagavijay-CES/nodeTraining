2a30cc34798b99484e91a48169e44828bc62a0f4
Nagavijay Kolla <tirumalanagavijay.kolla@cesltd.com>
2018-10-11 21:28:13 +0530

    "Folder with node architecture"

---
+++ ces-corn/lib/__tests__/cesCorn.test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const cesCorn = require('../index.js');
+
+describe('cesCorn', () => {
+  it('has a test', () => {
+    assert(false, 'cesCorn should have a test');
+  });
+});
